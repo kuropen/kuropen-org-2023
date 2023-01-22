465a9846463675b279a92dfecb218b67d9c8418b
Hirochika Yuda <hyuda@outlook.jp>
2023-01-23 00:46:25 +0900

    doc: Fix License Notice

--- apps/kporg-frontend/src/components/FediverseAccountList/ClipboardButton.tsx
+++ apps/kporg-frontend/src/components/FediverseAccountList/ClipboardButton.tsx
@@ -2,6 +2,8 @@
  * Copyright (C) 2023 Kuropen.
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This file includes Heroicons (https://heroicons.com/) which is licensed under MIT License.
  */
 
 import React, { useEffect, useState } from "react"
