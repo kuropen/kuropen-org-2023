fce049377911a5f90da0888e1ad537a1a7daf763
Hirochika Yuda <hyuda@outlook.jp>
2023-04-10 19:49:01 +0900

    Make it runnable on Vercel

--- apps/kporg-frontend/astro.config.mjs
+++ apps/kporg-frontend/astro.config.mjs
@@ -10,7 +10,12 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://kuropen.org',
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
